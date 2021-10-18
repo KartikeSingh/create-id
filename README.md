d538795f9ef52e19f86348fa8cae346320540bf3
KrazyDev <67275060+KartikeSingh@users.noreply.github.com>
2021-10-18 13:09:18 +0530

    hello there 👋

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# create-id
-generate random id?
+# Installations
+```
+npm i create-id
+```
+
+# What ?
+An module to create random IDs
+
+# Why ?
+- Fast
+- Customizable
+
+# How ?
+
+```js
+// Importing for Node JS
+const { randomID } = require('create-id');
+import { randomID } from 'create-id'; // for typescript
+
+// filters
+const length = 18; // length of the id
+const type = "letter" || ["letter", "number","symbol"]; // types of symbol to use for our id
+
+// Create a random ID
+const id = randomID();
+const id2 = randomID(length, type);
+```
+
+# Support
+for support or issues or queries contace me on my [discord server](https://discord.gg/XYnMTQNTFh) or create a issue [here](https://github.com/KartikeSingh/create-id/issues).
---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.randomID = void 0;
+const STRING = "QWERTYUIOPASDFGHJKLZXCVBNM", STRING2 = STRING.toLowerCase(), NUMBER = "1234567890", SYMBOLS = "!@#$%^&*";
+function randomID(length = 10, type = ["letter", "number"]) {
+    if (typeof (type) === "string" && (type !== "letter" && type !== "number" && type !== "symbol"))
+        throw new Error("Type should be either 'number' or 'letter', 'symbol' otherwise a array which contains these strings, but we got " + JSON.stringify(type));
+    Array.isArray(type) && type.forEach((type, i) => { if (type !== "letter" && type !== "number" && type !== "symbol")
+        throw new Error(`Array of type should conatiner string equal to either 'number' or 'letter', 'symbol' but we got ${JSON.stringify(type)} at index : ${i}`); });
+    let id = "", data = "";
+    Array.isArray(type) ? type.forEach(v => data += v === "symbol" ? SYMBOLS : v === "letter" ? STRING + STRING2 : NUMBER) : data += type === "symbol" ? SYMBOLS : type === "letter" ? STRING + STRING2 : NUMBER;
+    while (id.length < length)
+        id += data[Math.floor(Math.random() * data.length)];
+    return id;
+}
+exports.randomID = randomID;
