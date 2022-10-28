cbd94dce389da9e8e9e2259b24e6662751f9304e
null8626 <vierofernando9@gmail.com>
2022-10-28 13:50:19 +0700

    fix: fix index not defined

--- source/index.ts
+++ source/index.ts
@@ -1,15 +1,75 @@
-const STRING = "QWERTYUIOPASDFGHJKLZXCVBNM", STRING2 = STRING.toLowerCase(), NUMBER = "1234567890", SYMBOLS = "!@#$%^&*";
+// @ts-ignore
+import { randomBytes } from "node:crypto";
+
+const STRING = [
+  'Q', 'W', 'E', 'R', 'T', 'Y',
+  'U', 'I', 'O', 'P', 'A', 'S',
+  'D', 'F', 'G', 'H', 'J', 'K',
+  'L', 'Z', 'X', 'C', 'V', 'B',
+  'N', 'M', 
+  'q', 'w', 'e', 'r', 't', 'y',
+  'u', 'i', 'o', 'p', 'a', 's',
+  'd', 'f', 'g', 'h', 'j', 'k',
+  'l', 'z', 'x', 'c', 'v', 'b',
+  'n', 'm'
+];
+
+const NUMBER = [
+  '0', '1', '2', '3',
+  '4', '5', '6', '7',
+  '8', '9'
+];
+
+const SYMBOLS = [
+  '!', '@', '#',
+  '$', '%', '^',
+  '&', '*'
+];
 
 export function randomID(length = 10, type: randomType | randomType[] = ["letter", "number"]): string {
-    if (typeof (type) === "string" && (type !== "letter" && type !== "number" && type !== "symbol")) throw new Error("Type should be either 'number' or 'letter', 'symbol' otherwise a array which contains these strings, but we got " + JSON.stringify(type));
-    Array.isArray(type) && type.forEach((type, i) => { if (type !== "letter" && type !== "number" && type !== "symbol") throw new Error(`Array of type should conatiner string equal to either 'number' or 'letter', 'symbol' but we got ${JSON.stringify(type)} at index : ${i}`); })
-    let id = "", data = "";
+    let data: string[] = [];
 
-    Array.isArray(type) ? type.forEach(v => data += v === "symbol" ? SYMBOLS : v === "letter" ? STRING + STRING2 : NUMBER) : data += type === "symbol" ? SYMBOLS : type === "letter" ? STRING + STRING2 : NUMBER;
-
-    while (id.length < length) id += data[Math.floor(Math.random() * data.length)];
-
-    return id;
+    if (typeof type === "string") {
+		switch (type) {
+			case "number":
+				data.push(...NUMBER);
+				break;
+			case "letter":
+				data.push(...STRING);
+				break;
+			case "symbol":
+				data.push(...SYMBOLS);
+				break;
+			default:
+				throw new Error("Type should be either 'number' or 'letter', 'symbol' otherwise a array which contains these strings, but we got " + JSON.stringify(type));
+		}
+	} else if (!Array.isArray(type)) {
+		throw new TypeError("Expected a string or an array");
+	} else {
+		for (const t of type) {
+			switch (t) {
+			case "number":
+				if (!data.includes("0"))
+					data.push(...NUMBER);
+				
+				break;
+			case "letter":
+				if (!data.includes("A"))
+					data.push(...NUMBER);
+				
+				break;
+			case "symbol":
+				if (!data.includes("!"))
+					data.push(...SYMBOLS);
+				
+				break;
+			default:
+				throw new Error(`Array of type should contain string equal to either 'number' or 'letter', 'symbol' but we got ${JSON.stringify(t)}`);
+			}
+		}
+	}
+    
+    return [...randomBytes(length)].reduce((a, b) => a + data[Math.floor(b % data.length)], "");
 }
 
 type randomType = "letter" | "number" | "symbol";
